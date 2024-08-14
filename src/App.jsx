a3decc635a32d5b2910510add581a72b5dda4089
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-14 17:31:08 +0200

    Social-Media-Dashboard-main

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import EngagementComponent from "./components/EngagementComponent/Engagement";
+
+function App() {
+  const [followers, setFollowers] = useState([
+    {
+      id: 1,
+      platform: "/images/icon-facebook.svg",
+      user: "@nathanf",
+      metric: "Followers",
+      value: 1987,
+      change: "+12 Today",
+      borderColor: "hsl(208, 92%, 53%) 4px solid",
+    },
+    {
+      id: 2,
+      platform: "/images/icon-twitter.svg",
+      user: "@nathanf",
+      metric: "Followers",
+      value: 1044,
+      change: "+99 Today",
+      borderColor: "hsl(203, 89%, 53%) 4px solid",
+    },
+    {
+      id: 3,
+      platform: "/images/icon-instagram.svg",
+      user: "@realnathanf",
+      metric: "Followers",
+      value: "11k",
+      change: "+1099 Today",
+      borderColor: "transparent 4px solid",
+      borderColorGradient:
+        "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%)) 1",
+    },
+    {
+      id: 4,
+      platform: "/images/icon-youtube.svg",
+      user: "Nathan F.",
+      metric: "Subscribers",
+      value: 8239,
+      change: "-144 Today",
+      borderColor: "hsl(348, 97%, 39%) 4px solid",
+    },
+  ]);
+
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/followers') .then((response) => response.json()) .then((data) => setFollowers(data))}, [])
+
+  function toggleDarkMode() {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    localStorage.setItem("darkMode", JSON.stringify(newMode));
+  }
+
+  return (
+    <div
+      className={
+        isDarkMode ? "dark-mode-main-container" : "light-mode-main-container"
+      }
+    >
+      <header>
+        <div className="title-followers">
+          <h1>Social Media Dashboard</h1>
+          <p>Total Followers: 23,004</p>
+        </div>
+        <hr></hr>
+        <div id="darkMode">
+          <p>Dark Mode</p>
+          <div id="darkModeBTN">
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+              />
+              <span className="slider"></span>
+            </label>
+          </div>
+        </div>
+      </header>
+
+      <div className="followers-wrapper">
+        {followers.map((item) => {
+          const isPositive = item.change.includes("+");
+          const chanValue = item.change.replace(/^\+|^-/, "").trim();
+          const style = {
+            borderTop: item.borderColor,
+            borderImage: item.borderColorGradient,
+          };
+
+          return (
+            <div
+              key={item.id}
+              className={`${item.className} ${
+                isDarkMode ? "dark-card" : "light-card"
+              }`}
+              style={style}
+            >
+              <div id="title1">
+                <img src={item.platform} alt="not-found" />
+                <p>{item.user}</p>
+              </div>
+              <div>
+                <h1>{item.value}</h1>
+                <p id="metric">{item.metric}</p>
+              </div>
+              <span
+                id="change"
+                style={{ color: isPositive ? "#1db489" : "#dc414c" }}
+              >
+                {isPositive ? "▲" : "▼"} {chanValue}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+
+      <EngagementComponent isDarkMode={isDarkMode} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EngagementComponent/Engagement.jsx
@@ -0,0 +1,113 @@
+import "./Engagement.css";
+import { useState, useEffect } from "react";
+
+function EngagementComponent(props) {
+  const [engagement, setEngagement] = useState([
+    {
+      id: 1,
+      platform: "/images/icon-facebook.svg",
+      metric: "Page Views",
+      value: 87,
+      change: "+3%",
+    },
+    {
+      id: 2,
+      platform: "/images/icon-facebook.svg",
+      metric: "Likes",
+      value: 52,
+      change: "-2%",
+    },
+    {
+      id: 3,
+      platform: "/images/icon-instagram.svg",
+      metric: "Likes",
+      value: 5462,
+      change: "+2257%",
+    },
+    {
+      id: 4,
+      platform: "/images/icon-instagram.svg",
+      metric: "Profile Views",
+      value: "52k",
+      change: "+1375%",
+    },
+    {
+      id: 5,
+      platform: "/images/icon-twitter.svg",
+      metric: "Retweets",
+      value: 117,
+      change: "+303%",
+    },
+    {
+      id: 6,
+      platform: "/images/icon-twitter.svg",
+      metric: "Likes",
+      value: 507,
+      change: "+553%",
+    },
+    {
+      id: 7,
+      platform: "/images/icon-youtube.svg",
+      metric: "Likes",
+      value: 107,
+      change: "-19%",
+    },
+    {
+      id: 8,
+      platform: "/images/icon-youtube.svg",
+      metric: "Total Views",
+      value: 1407,
+      change: "-12%",
+    },
+  ]);
+  // useEffect(() => {fetch('http://localhost:3000/engagement') .then(response => response.json()) .then(data => setEngagement(data)) }, [])
+
+  return (
+    <div
+      id="engagement-section"
+      className={
+        props.isDarkMode ? "dark-mode-engagement" : "light-mode-engagement"
+      }
+    >
+      <h2 id="engagement-title">Overview - Today</h2>
+      <div id="engagement-wrapper">
+        {engagement.map((ele) => {
+          const isPositive = ele.change.includes("+");
+          const chanValue = ele.change.replace(/^\+|^-/, "").trim();
+
+          return (
+            <div key={ele.id}>
+              <div
+                id="engagement-card"
+                className={
+                  props.isDarkMode
+                    ? "dark-mode-engagement-card"
+                    : "light-mode-engagement-card"
+                }
+              >
+                <div id="platformTitle">
+                  <p>{ele.metric}</p>
+                  <img src={ele.platform} alt="not" />
+                </div>
+
+                <div id="valueChange">
+                  <h2>{ele.value}</h2>
+                  <span
+                    id="change-engagement"
+                    className={ele.className}
+                    style={{ color: isPositive ? "#1db489" : "#dc414c" }}
+                  >
+                    {isPositive ? "▲" : "▼"}
+                    {chanValue}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default EngagementComponent;
